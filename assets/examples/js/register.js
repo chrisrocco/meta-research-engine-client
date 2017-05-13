b7960d1aedd88faf769bea484b268ffc94c7385b
Chris Rocco <chris.rocco7@gmail.com>
2017-05-12 23:11:50 -0500

    Updated registration page

--- assets/examples/js/register.js
+++ assets/examples/js/register.js
@@ -15,19 +15,40 @@
 
 
 function register(){
-    var name = $("#name").val();
-    var tmp = name.split(" ");
 
-    var firstName = tmp[0];
-    var lastName = tmp[1];
+    var name = $("#name").val();
     var email = $("#email").val();
     var password = $("#password").val();
     var confirmPassword = $("#confirmPassword").val();
 
+    /* Validate Name */
+    var tmp = name.split(" ");
+    if( tmp.length < 2 ){
+        alert("Please provide your full name");
+        return false;
+    }
+
+    var firstName = tmp[0];
+    var lastName = tmp[1];
+
+    /* Validate Email */
+    /* Validate Password */
+    if( password !== confirmPassword ){
+        alert("Passwords do not match");
+        return false;
+    }
+
     var promise = AuthService.register( firstName, lastName, email, password );
-    promise.success( function( data ){
-        alert("Registration Successful");
-        window.location = "login.html";
+    promise.complete( function( response ){
+        switch( response.status ){
+            case 200:
+                alert("Registration Successful. You may now login.");
+                window.location = "login.html";
+                break;
+            case 409:
+                alert("That email already exists. Do you need to login?");
+                break;
+        }
     } );
 
     return false;
