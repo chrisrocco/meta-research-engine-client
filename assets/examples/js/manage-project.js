4ed6d846cdd8a059d1dfd87e26f539895d56c48a
Chris Rocco <chris.rocco7@gmail.com>
2017-05-19 19:41:48 -0500

    Implemented Manage Project Activity

--- assets/examples/js/manage-project.js
+++ assets/examples/js/manage-project.js
@@ -12,29 +12,10 @@
         Site.run();
         init();
     });
-
-
-    // Custom filter UI
-    // ----------
-    (function () {
-        $('#exampleCustomFilter').footable();
-        $('.filter-ui-status').on('change', function () {
-            var filtering = FooTable.get('#exampleCustomFilter').use(FooTable.Filtering), // get the filtering component for the table
-                filter = $(this).val(); // get the value to filter by
-            if (filter === 'none') { // if the value is "none" remove the filter
-                filtering.removeFilter('status');
-            } else { // otherwise add/update the filter.
-                filtering.addFilter('status', filter, ['status']);
-            }
-            filtering.filter();
-        });
-    })();
-
-
 })(document, window, jQuery);
 
 
-function submitPaperUploadForm( formElement ) {
+function submitPaperUploadForm(  ) {
 
     var formData = new FormData();
     var file = $("#papersCSV")[0].files[0];
@@ -42,7 +23,27 @@ function submitPaperUploadForm( formElement ) {
 
     var promise = DataService.uploadPapersCSV( localStorage.projectKey, formData );
     promise.success( function ( data ) {
-        console.log("from server", data);
+        console.log("success from server", data);
+    });
+    promise.error( function( response ) {
+        var badColumns = "columnCountError";
+        var badFile    = "parseFailure";
+        var empty      = "emptyFileError";
+
+        var error = response.responseJSON;
+        switch ( error.reason ){
+            case badColumns:
+                alert("Invalid column count on line " + error.row );
+                break;
+            case badFile:
+                alert("Could not parse csv");
+                break;
+            case empty:
+                alert("Empty File");
+                break;
+        }
+
+        console.log("fail from server", error);
     });
 
     return false;
@@ -52,11 +53,51 @@ function init( ){
     var p = DataService.loadManageProject( localStorage.projectKey );
     p.success( function( data ){
         var projectObject = data.project;
-        var paperQueue = data.paperQueue;
+        var papers = data.papers;
+        console.log( "Project Object", projectObject );
+        console.log( "Papers", papers );
 
         $("[data-projectname]").html( projectObject.name );
-
-        console.log( "Project Object", projectObject );
-        console.log( "Paper Queue", paperQueue );
+        var outlet = document.querySelector("#templateOutlet");
+        var template = document.querySelector("#paperTemplate").content;
+        for (var i = 0; i < papers.length; i++) {
+            var paper = papers[i];
+            /* server not ready */
+            paper.status = 'active';
+            /* server not ready */
+            template.querySelector("[data-title]").textContent = paper.title;
+            template.querySelector("[data-desc]").textContent = paper.description.substring(0, 33) + "..";
+            template.querySelector("[data-assignmentCount]").textContent = paper.assignmentCount;
+            template.querySelector("[data-status]").textContent = paper.status;
+            outlet.appendChild( template.cloneNode( true ) );
+        }
+        initFooTable();
+        initDropify();
     })
+}
+
+function initFooTable(){
+    $('#papersTable').footable( );
+    $('.filter-ui-status').on('change', function () {
+        var filtering = FooTable.get('#papersTable').use(FooTable.Filtering), // get the filtering component for the table
+            filter = $(this).val(); // get the value to filter by
+        if (filter === 'none') { // if the value is "none" remove the filter
+            filtering.removeFilter('status');
+        } else { // otherwise add/update the filter.
+            filtering.addFilter('status', filter, ['status']);
+        }
+        filtering.filter();
+    });
+}
+
+function initDropify(){
+    $('#papersCSV').dropify({
+        messages: {
+            'default': 'Drag and drop a file here or click',
+            'replace': 'Drag and drop or click to replace',
+            'remove':  'Remove',
+            'error':   'Ooops, something is off.'
+        },
+        showErrors: false
+    });
 }
