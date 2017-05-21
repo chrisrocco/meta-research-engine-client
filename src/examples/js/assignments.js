3cd86fceaa3bf8cb1fa98a243594d1417290e950
Chris Rocco <chris.rocco7@gmail.com>
2017-05-20 21:08:38 -0500

    paper status rendering

--- src/examples/js/assignments.js
+++ src/examples/js/assignments.js
@@ -28,15 +28,39 @@ function loadAssignments(){
         console.log( "server", data );
         for( var i = 0; i < data.length; i++ ) {
             var obj = data[i];
+            var paper           =   obj.paper;
+            var assignment      =   obj.assignment;
+            var study           =   obj.study;
 
-            /* set the content object DOM with selectors above */
-            content.querySelector( datastatus ).textContent = "active";
-            content.querySelector( datapapername ).textContent = obj.paper.title;
-            content.querySelector( datapapermeta ).textContent = "PMC-ID: " + obj.paper.pmcID;
+            var statusClass = "badge badge-primary";
+            switch( paper.status ){
+                case "active":
+                    statusClass = "badge badge-primary";
+                    break;
+                case "pending":
+                    statusClass = "badge badge-warning";
+                    break;
+                case "conflicted":
+                    statusClass = "badge badge-danger";
+                    break;
+            }
+
+            /**
+             * RENDER INFORMATION
+             */
+            // Setting status button
+            content.querySelector( datastatus ).className = statusClass;
+            content.querySelector( datastatus ).textContent = paper.status;
+            // Setting paper information
+            content.querySelector( datapapername ).textContent = paper.title;
+            content.querySelector( datapapermeta ).textContent = "PMC-ID: " + paper.pmcID;
+            // Setting progress bar
             content.querySelector( dataprogress ).style.width = obj.assignment.completion+"%";
             content.querySelector( dataprogresstext ).textContent = obj.assignment.completion+"%";
+            // Setting Study information
             content.querySelector( datastudyname ).textContent = obj.project.name;
             content.querySelector( datastudymeta ).textContent = obj.project.date_created;
+            // Setting action button
             content.querySelector( databutton ).dataset.assignmentKey = obj.assignment._key;
 
             outlet.appendChild( content.cloneNode( true ) );
