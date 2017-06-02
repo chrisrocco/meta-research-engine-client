006d76b204ea77d0f5ea81dd46653b3bfa89dd99
Chris Rocco <chris.rocco7@gmail.com>
2017-06-02 15:24:24 -0500

    Master Settings

--- assets/js/URLs.js
+++ assets/js/URLs.js
@@ -17,10 +17,10 @@
         value: true
     });
     var urls = {};
-    urls['api'] = "https://dev.researchcoder.com/api";
+    urls['api'] = "https://researchcoder.com/api";
     urls['login'] = urls['api'] + "/users/login";
     urls['register'] = urls['api'] + "/users/register";
-    urls['resetPasswordCallback'] = "https://dev.researchcoder.com/reset-password.html";
+    urls['resetPasswordCallback'] = "https://researchcoder.com/reset-password.html";
 
     function getUrl(name) {
         return urls[name];
