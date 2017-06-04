d469d2d65954e7d1b4648758fa9dd0a16d9e21db
Chris Rocco <chris.rocco7@gmail.com>
2017-06-04 17:25:03 -0500

    updated project center

--- assets/js/DataService.js
+++ assets/js/DataService.js
@@ -164,8 +164,8 @@
     function http(config) {
         config['url'] = API_BASE_PATH + config['url'];
         config['headers'] = {
-            "Authorization": "Bearer " + AuthService.getToken() // token here
-        };
+            "Authorization": "Bearer " + AuthService.getToken // token here
+            () };
         config['statusCode'] = {
             500: reportError
         };
