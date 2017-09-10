d67bc456ca08c56fbb79f513b02b0944f18551fb
Chris Rocco <chris.rocco7@gmail.com>
2017-09-10 12:59:10 -0500

    moved js inline

--- forgot-password.php
+++ forgot-password.php
@@ -1,53 +1,105 @@
 <!DOCTYPE html>
 <html class="no-js css-menubar" lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-  <meta name="description" content="Research Study Encoding System">
-  <meta name="author" content="Chris Rocco">
-  <title>Forgot password | Big Data UAB</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Research Study Encoding System">
+    <meta name="author" content="Chris Rocco">
+    <title>Forgot password | Big Data UAB</title>
 
-  <?php require 'assets/partials/css-includes.html'; ?>
+    <?php require 'assets/partials/css-includes.html'; ?>
 
 </head>
 <body class="animsition page-forgot-password layout-full">
-  <!--[if lt IE 8]>
-        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
-  <!-- Page -->
-  <div class="page vertical-align text-center" data-animsition-in="fade-in" data-animsition-out="fade-out">
+<?php require 'assets/partials/IE-check.html'; ?>
+<!-- Page -->
+<div class="page vertical-align text-center" data-animsition-in="fade-in" data-animsition-out="fade-out">
     <div class="page-content vertical-align-middle animation-slide-top animation-duration-1">
-      <h2>Forgot Your Password ?</h2>
-      <p>Input your registered email to reset your password</p>
-      <form method="post" role="form" onsubmit="return submitRecoverForm()">
-        <div class="form-group">
-          <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Your Email">
-        </div>
-        <div class="form-group">
-          <button type="submit" class="btn btn-primary btn-block">Reset Your Password</button>
-        </div>
-      </form>
-      <footer class="page-copyright">
-        <p>WEBSITE BY Chris Rocco</p>
-        <p>© 2017. All RIGHT RESERVED.</p>
-        <div class="social">
-          <a class="btn btn-icon btn-pure" href="javascript:void(0)">
-            <i class="icon bd-twitter" aria-hidden="true"></i>
-          </a>
-          <a class="btn btn-icon btn-pure" href="javascript:void(0)">
-            <i class="icon bd-facebook" aria-hidden="true"></i>
-          </a>
-          <a class="btn btn-icon btn-pure" href="javascript:void(0)">
-            <i class="icon bd-dribbble" aria-hidden="true"></i>
-          </a>
-        </div>
-      </footer>
+        <h2>Forgot Your Password ?</h2>
+        <p>Input your registered email to reset your password</p>
+        <form method="post" role="form" onsubmit="return submitRecoverForm()">
+            <div class="form-group">
+                <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Your Email">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block">Reset Your Password</button>
+            </div>
+        </form>
+        <footer class="page-copyright">
+            <p>WEBSITE BY Chris Rocco</p>
+            <p>© 2017. All RIGHT RESERVED.</p>
+            <div class="social">
+                <a class="btn btn-icon btn-pure" href="javascript:void(0)">
+                    <i class="icon bd-twitter" aria-hidden="true"></i>
+                </a>
+                <a class="btn btn-icon btn-pure" href="javascript:void(0)">
+                    <i class="icon bd-facebook" aria-hidden="true"></i>
+                </a>
+                <a class="btn btn-icon btn-pure" href="javascript:void(0)">
+                    <i class="icon bd-dribbble" aria-hidden="true"></i>
+                </a>
+            </div>
+        </footer>
     </div>
-  </div>
-  <!-- End Page -->
-  <?php require 'assets/partials/js-includes.html'; ?>
+</div>
+<!-- End Page -->
+<?php require 'assets/partials/js-includes.html'; ?>
 
-  <script src="/assets/examples/js/forgot-password.js"></script>
+<script>
+    $(document).ready(function ($) {
+        Site.run();
+    });
+
+    function submitRecoverForm() {
+        var form = document.querySelector("form");
+        var email = form.email.value;
+
+        console.log(email);
+
+        var promise = DataService.postForgotPassword(email);
+        promise.success(function (res) {
+            swal({
+                    title: "Success!",
+                    text: "An email with recovery instructions has been sent.",
+                    type: "success",
+                    showCancelButton: false,
+                    confirmButtonClass: "btn-success",
+                    confirmButtonText: 'OK',
+                    closeOnConfirm: false
+                },
+                function () {
+                    window.location = "index.html"
+                });
+            console.log("server response", res);
+        });
+        promise.error(function (res) {
+            if (res.status == 404) {
+                swal({
+                    title: "What now?",
+                    text: "I don't even recognize that email!",
+                    type: "error",
+                    showCancelButton: false,
+                    confirmButtonClass: "btn-danger",
+                    confirmButtonText: 'OK',
+                    closeOnConfirm: false
+                });
+                return;
+            }
+            swal({
+                title: "Ummm..",
+                text: "Something went wrong. Sorry.",
+                type: "error",
+                showCancelButton: false,
+                confirmButtonClass: "btn-danger",
+                confirmButtonText: 'OK',
+                closeOnConfirm: false
+            });
+            console.log("fail from server", res);
+        });
+
+        return false;
+    }
+</script>
 </body>
 </html>
--- login.php
+++ login.php
@@ -13,9 +13,10 @@
         <div class="panel">
             <div class="panel-body">
                 <div class="brand">
-                    <img class="brand-img" src="/assets/images/UAB-logo.png" style="width: 100%" alt="UAB Research Coder">
+                    <img class="brand-img" src="/assets/images/UAB-logo.png" style="width: 100%"
+                         alt="UAB Research Coder">
                 </div>
-                <form action="#" onsubmit="return login()">
+                <form onsubmit="return login()">
                     <div class="form-group form-material floating" data-plugin="formMaterial">
                         <input type="email" class="form-control" name="email" id="emailInput"/>
                         <label class="floating-label">Email</label>
@@ -42,6 +43,59 @@
 <?php require 'assets/partials/footer.html'; ?>
 <!-- End Page -->
 <?php require 'assets/partials/js-includes.html'; ?>
-<script src="/assets/examples/js/login.js"></script>
+<script>
+    (function (document, window, $) {
+        'use strict';
+
+        var Site = window.Site;
+        $(document).ready(function () {
+            Site.run();
+        });
+
+    })(document, window, jQuery);
+
+
+    function login() {
+        AuthService.login(
+            $("#emailInput").val(),
+            $("#passwordInput").val()
+        ).then(
+            function () {
+                window.location = "assignments.php";
+            },
+            function fail(response) {
+                console.log("fail", response);
+                var data = response.responseJSON;
+                data.reason = data.reason.toUpperCase();
+
+                if (data.reason === "INACTIVE") {
+                    swal({
+                        title: "Not so Fast!",
+                        text: "You need to validate your email first.",
+                        type: "warning",
+                        showCancelButton: false,
+                        confirmButtonClass: "btn-warning",
+                        confirmButtonText: 'OK',
+                        closeOnConfirm: false
+                    });
+                }
+                if (data.reason === "INVALID") {
+                    swal({
+                        title: "Invalid Login",
+                        text: "Try Again",
+                        type: "error",
+                        showCancelButton: false,
+                        confirmButtonClass: "btn-danger",
+                        confirmButtonText: 'OK',
+                        closeOnConfirm: false
+                    });
+                }
+            }
+        );
+
+        return false;
+    }
+
+</script>
 </body>
 </html>
