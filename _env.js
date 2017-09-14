1517bb08cf7715c762b22813c9fcf7df8bfbe32a
Chris Rocco <chris.rocco7@gmail.com>
2017-09-13 21:00:09 -0500

    env.js => _env.js

---
+++ _env.js
@@ -0,0 +1,9 @@
+(function(){
+    window._env = {
+        "skin": "uab",
+        "serverURL": "https://researchcoder.com/api",
+        "loginURL": "https://researchcoder.com/api/users/login",
+        "registerURL": "https://researchcoder.com/api/users/register",
+        "renewURL": "https://researchcoder.com/api/renewToken"
+    };
+})();
